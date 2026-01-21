73b9c5eba744ef45018aba27bb9aad4be6fcbf7a
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-01-21 11:50:55 -0500

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rollnconnect.balloon',
+  appName: 'Balloon Dating Game',
+  webDir: 'public',
+  bundledWebRuntime: false,
+};
+
+export default config;
